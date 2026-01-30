138fccb79830de57dbae7f97c100daf642932f27
krishna dasadiya <krishnadasadiya909@gmail.com>
2026-01-30 13:51:02 +0530

    [DK] [task file added]

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
   import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Register from './pages/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
+     
+      <Register/>
     </>
   )
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Register = () => {
+  return (
+    <>
+      <div className="form-container" >
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default Register;
