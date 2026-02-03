2a4aca6481aef9814e4f12b5fef47a6d5e89e227
krishna dasadiya <krishnadasadiya909@gmail.com>
2026-02-03 08:19:31 +0530

    [DK] [REGISTRATION]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, NavLink, RouterProvider } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import './App.css'
 
+const DefaultRoute=()=>{
+  const authData=JSON.parse(localStorage.getItem('authData'));
+  if(authData){
+    return <Navigate to='/login' replace/>
+  }
+  return <Navigate to='/register' replace/>
+}
 function App() {
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <DefaultRoute />
+    },
     {
       path: "/login",
       element: <Login />
