eb55d6e5aa5e7f00fae173c813f87eef18df964e
krishna dasadiya <krishnadasadiya909@gmail.com>
2026-02-04 13:42:13 +0530

    [DK] [Navbar place]

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import TaskList from "../components/TaskList";
 const Dashboard = () => {
-  return (
-    <div><h1>Welcome to Dashboard</h1></div>
-  )
-}
+  const navigate = useNavigate();
+  const [tasks, setTasks] = useState([]);
 
-export default Dashboard
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = response.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    localStorage.removeItem("authData");
+    // localStorage.clear()
+    navigate("/login");
+  };
+  return (
+    <div>
+      <Navbar title="Task Management" onLogout={handleLogout} />
+      <h1>Welcome to Dashboard</h1>
+      <h1>MY TASKS</h1>
+      <TaskList />
+    </div>
+  );
+};
+
+export default Dashboard;
